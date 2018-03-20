0aab035f3cac076d54b86c3e9efd7b023bfccd96
Ryan Hu <ryanhuuu@gmail.com>
2018-03-20 01:14:36 +0000

    automatically backed up by learn

--- lib/runner.rb
+++ lib/runner.rb
@@ -1,11 +1,2 @@
 require_relative "blackjack.rb"
-  welcome()
-  total = initial_round()
-  until (total > 21) do
-    hit?()
-    total = hit?();
-    display_card_total
-  end 
-  
-  end_game()
 runner
